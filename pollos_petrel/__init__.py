f147399e54da592ea419d1016a2161db84cccf98
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-06-13 10:35:59 -0700

    ✅ Add production code from 3 class

---
+++ pollos_petrel/__init__.py
@@ -0,0 +1,6 @@
+"A module to pollos_petrel"
+
+__version__ = "0.1.0"
+from .petrel_age_predictor import *  # noqa
+from .error_estimator_function import *  # noqa
+from .calculate_mass_loss_rates import *  # noqa
