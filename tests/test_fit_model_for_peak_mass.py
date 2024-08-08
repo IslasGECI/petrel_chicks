ba32ab2c0b380ffb90aa8828dcd139bdc7383238
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-08-08 21:55:45 +0000

    🛑🧪 Fail tests

--- tests/test_fit_model_for_peak_mass.py
+++ tests/test_fit_model_for_peak_mass.py
@@ -3,6 +3,7 @@ from petrel_chicks.fit_model_for_peak_mass import (
 )
 
 import pandas as pd
+import numpy as np
 
 
 ages_and_mass = pd.DataFrame({"ID": ["a", "a", "a"], "Masa": [5, 8, 0], "Edad": [0, 1, -1]})
@@ -12,3 +13,9 @@ def test_find_age_for_max_mass_from_data():
     obtained_age = find_age_for_max_mass_from_data(ages_and_mass)
     expected_age = 2
     assert obtained_age == expected_age
+
+    ages_and_mass_with_na = pd.DataFrame(
+        {"ID": ["a", "a", "a", "b"], "Masa": [8, 21, 0, np.NaN], "Edad": [0, 1, -1, 3]}
+    )
+    obtained_age = find_age_for_max_mass_from_data(ages_and_mass_with_na)
+    assert isinstance(obtained_age, int)
