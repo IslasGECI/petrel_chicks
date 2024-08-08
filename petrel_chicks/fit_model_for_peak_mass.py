3d5a787d692de9998a72168f33231a179771bd53
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-08-08 21:02:52 +0000

    ✅ Pass tests

--- petrel_chicks/fit_model_for_peak_mass.py
+++ petrel_chicks/fit_model_for_peak_mass.py
@@ -1,6 +1,10 @@
 from scipy.optimize import curve_fit
 
 
+def find_age_for_max_mass(parameters):
+    return -parameters[1] / (2 * parameters[0])
+
+
 def fit_model_age_vs_mass(ages_and_mass):
     return curve_fit(polinomio, ages_and_mass.Edad, ages_and_mass.Masa)
 
