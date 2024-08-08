3d7c15addf8b2bbfcf638ef98764548334efcac6
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-08-08 20:35:18 +0000

    🛑🧪 Fail tests

--- tests/test_fit_model_for_peak_mass.py
+++ tests/test_fit_model_for_peak_mass.py
@@ -5,4 +5,6 @@ import pandas as pd
 
 def test_fit_model_age_vs_mass():
     ages_and_mass = pd.DataFrame({"ID": ["a", "a", "a"], "Masa": [5, 8, 0], "Edad": [0, 1, -1]})
-    obtained_model = fit_model_age_vs_mass(ages_and_mass)
+    obtained_parameters, _ = fit_model_age_vs_mass(ages_and_mass)
+    expected_parameters = [5, 4, -1]
+    assert obtained_parameters == expected_parameters
