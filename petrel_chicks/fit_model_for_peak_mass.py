ac4b64d184abd07584a9af0a2638d670830b1bf8
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-08-08 20:42:17 +0000

    ✅ Pass tests

--- petrel_chicks/fit_model_for_peak_mass.py
+++ petrel_chicks/fit_model_for_peak_mass.py
@@ -1,2 +1,9 @@
+from scipy.optimize import curve_fit
+
+
 def fit_model_age_vs_mass(ages_and_mass):
-    pass
+    return curve_fit(polinomio, ages_and_mass.Edad, ages_and_mass.Masa)
+
+
+def polinomio(x, a, b, c):
+    return a * x**2 + b * x + c
--- tests/test_fit_model_for_peak_mass.py
+++ tests/test_fit_model_for_peak_mass.py
@@ -6,5 +6,5 @@ import pandas as pd
 def test_fit_model_age_vs_mass():
     ages_and_mass = pd.DataFrame({"ID": ["a", "a", "a"], "Masa": [5, 8, 0], "Edad": [0, 1, -1]})
     obtained_parameters, _ = fit_model_age_vs_mass(ages_and_mass)
-    expected_parameters = [5, 4, -1]
-    assert obtained_parameters == expected_parameters
+    expected_parameters = [-1, 4, 5]
+    assert all(obtained_parameters == expected_parameters)
