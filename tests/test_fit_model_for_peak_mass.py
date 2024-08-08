22c0bdcd537922f34214b3c8877ce08e98b6e6b5
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-08-08 20:49:57 +0000

    🛑🧪 Fail tests

--- tests/test_fit_model_for_peak_mass.py
+++ tests/test_fit_model_for_peak_mass.py
@@ -1,4 +1,4 @@
-from petrel_chicks.fit_model_for_peak_mass import fit_model_age_vs_mass
+from petrel_chicks.fit_model_for_peak_mass import fit_model_age_vs_mass, find_age_for_max_mass
 
 import pandas as pd
 
@@ -8,3 +8,10 @@ def test_fit_model_age_vs_mass():
     obtained_parameters, _ = fit_model_age_vs_mass(ages_and_mass)
     expected_parameters = [-1, 4, 5]
     assert all(obtained_parameters == expected_parameters)
+
+
+def tests_find_age_for_max_mass():
+    parameters = [-1, 4, 5]
+    obtained_age = find_age_for_max_mass(parameters)
+    expected_age = 2
+    assert obtained_age == expected_age
