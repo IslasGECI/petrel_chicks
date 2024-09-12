6f9b011f8a00a14eb7856e66eb5a9b2fd9b2156c
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-09-12 16:38:04 +0000

    ✅ Pass tests

--- petrel_chicks/__init__.py
+++ petrel_chicks/__init__.py
@@ -3,5 +3,6 @@
 __version__ = "0.2.1"
 from .calculate_mass_loss_rates import *  # noqa
 from .error_estimator_function import *  # noqa
+from .fiter_per_season import *  # noqa
 from .fit_model_for_peak_mass import *  # noqa
 from .petrel_age_predictor import *  # noqa
