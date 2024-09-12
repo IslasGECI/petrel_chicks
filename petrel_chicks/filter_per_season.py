076b4bc592a36f91a00cf77aaf47dc72fcb4b6eb
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-09-12 17:38:51 +0000

    🚚➕📦  extract code from pollos_petrel

---
+++ petrel_chicks/filter_per_season.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+
+def filterPerSeason(data):
+    seasons = data.Anio.unique()
+    dates = []
+    rings = []
+    recaptures = []
+
+    for i in seasons:
+        data_season = data[data.Anio == i]
+        dates.append(len(data_season.Fecha.unique()))
+        rings.append(len(data_season.ID_anillo.unique()))
+        duplicados = data_season["ID_anillo"].duplicated(keep="last")
+        recaptures.append(sum(duplicados))
+
+    data = {"temporada": seasons, "eventos": dates, "capturas": rings, "recapturas": recaptures}
+
+    dataframe = pd.DataFrame(data=data)
+    return dataframe
