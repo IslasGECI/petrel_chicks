5d6101b7a1f47018695a079315304e0bea357575
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-10-09 20:19:19 +0000

    ✅ Pass tests

--- petrel_chicks/plot_peak_mass_model.py
+++ petrel_chicks/plot_peak_mass_model.py
@@ -5,5 +5,6 @@ import matplotlib.pyplot as plt
 def plot_peak_mass_model_and_data(df):
     _, ax = gp.geci_plot()
 
+    ax_scatter = ax.scatter(df.Edad, df.Masa)
     plt.ylabel("Mass $\\left( g \\right)$")
-    return ax
+    return ax, ax_scatter
