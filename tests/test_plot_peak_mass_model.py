2ffd81de151ccd4d3e5b3837e7c99953fce6796f
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-10-09 20:00:58 +0000

    🛑🧪 Fail tests

--- tests/test_plot_peak_mass_model.py
+++ tests/test_plot_peak_mass_model.py
@@ -12,9 +12,10 @@ def test_plot_peak_mass_model():
             "Edad": [0, 1, -1, 2, 3],
         }
     )
-    obtained = plot_peak_mass_model_and_data(df)
-    assert isinstance(obtained, plt.axes._axes.Axes)
+    obtained_ax, obtained_plt = plot_peak_mass_model_and_data(df)
+    assert isinstance(obtained_ax, plt.axes._axes.Axes)
 
-    obtained_y_label = obtained.get_ylabel()
+    obtained_y_label = obtained_ax.get_ylabel()
     expected_y_label = "Mass $\\left( g \\right)$"
     assert obtained_y_label == expected_y_label
+    print(obtained_plt.lines[0])
