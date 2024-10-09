f50ec98de5f8d096421b5728cec8b2e123fbf9b2
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-10-09 20:22:54 +0000

    ♻️  Refactor

--- tests/test_plot_peak_mass_model.py
+++ tests/test_plot_peak_mass_model.py
@@ -12,10 +12,10 @@ def test_plot_peak_mass_model():
             "Edad": [0, 1, -1, 2, 3],
         }
     )
-    obtained_ax, obtained_plt = plot_peak_mass_model_and_data(df)
+    obtained_ax = plot_peak_mass_model_and_data(df)
     assert isinstance(obtained_ax, plt.axes._axes.Axes)
 
     obtained_y_label = obtained_ax.get_ylabel()
     expected_y_label = "Mass $\\left( g \\right)$"
     assert obtained_y_label == expected_y_label
-    assert (obtained_plt._offsets.data[:, 1] == df.Masa).all()
+    assert (obtained_ax._children[0]._offsets.data[:, 1] == df.Masa).all()
