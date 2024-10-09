e0ffb24391f7d808bc2882cd8a48f2fab50ab7e5
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-10-09 19:50:55 +0000

    🛑🧪 Fail tests

---
+++ tests/test_plot_peak_mass_model.py
@@ -0,0 +1,16 @@
+from petrel_chicks import plot_peak_mass_model_and_data
+import pandas as pd
+import matplotlib as plt
+
+
+def test_plot_peak_mass_model():
+    df = pd.DataFrame(
+        {
+            "ID_unico": ["c-2013", "c-2013", "c-2013", "c-2013", "c-2013"],
+            "Temporada": [2013, 2013, 2013, 2013, 2013],
+            "Masa": [5, 8, 0, 10, 8.1],
+            "Edad": [0, 1, -1, 2, 3],
+        }
+    )
+    obtained = plot_peak_mass_model_and_data(df)
+    assert isinstance(obtained, plt.axes._axes.Axes)
