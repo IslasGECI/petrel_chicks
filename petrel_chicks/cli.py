cfb13c718048fa54d3215e9887784ad268ba3133
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-10-10 21:57:24 +0000

    ✅ Pass tests

--- petrel_chicks/cli.py
+++ petrel_chicks/cli.py
@@ -7,6 +7,7 @@ cli = typer.Typer()
 def plot_peak_mass_model(
     data_path: str = typer.Option("", help="Input file path"),
     season: int = typer.Option("", help="Season"),
+    output_path: str = typer.Option("", help="Output file path"),
 ):
     pass
 
