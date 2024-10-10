0048fd4bd7c8cc4d819adbadcaf46b782ccd6f8d
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-10-10 21:54:09 +0000

    🛑🧪 Fail tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -5,5 +5,10 @@ runner = CliRunner()
 
 
 def tests_plot():
-    result = runner.invoke(cli, ["--help"])
+    result = runner.invoke(cli, ["version", "--help"])
     assert result.exit_code == 0
+
+    result = runner.invoke(cli, ["plot-peak-mass-model", "--help"])
+    assert result.exit_code == 0
+    assert " Input file path " in result.stdout
+    assert " Season " in result.stdout
