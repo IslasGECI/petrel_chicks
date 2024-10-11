8f8dad272f918c00ced9db1735cd9dacdf44c12e
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-10-11 16:21:36 +0000

    ✅ Pass tests

--- petrel_chicks/cli.py
+++ petrel_chicks/cli.py
@@ -1,4 +1,8 @@
+import matplotlib.pyplot as plt
+import pandas as pd
 import typer
+
+from petrel_chicks.plot_peak_mass_model import _plot_peak_mass_model_and_data
 
 cli = typer.Typer()
 
@@ -9,7 +13,9 @@ def plot_peak_mass_model(
     season: int = typer.Option("", help="Season"),
     output_path: str = typer.Option("", help="Output file path"),
 ):
-    pass
+    data = pd.read_csv(data_path)
+    _plot_peak_mass_model_and_data(data)
+    plt.savefig(output_path)
 
 
 @cli.command()
