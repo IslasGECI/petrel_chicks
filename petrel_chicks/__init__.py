15d1cd1e6b8ff7ff0ad9716dd24b13a94b63b709
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-10-11 17:26:36 +0000

    ♻️  Refactor

--- petrel_chicks/__init__.py
+++ petrel_chicks/__init__.py
@@ -7,8 +7,8 @@ from .error_estimator_function import *  # noqa
 from .filter_per_season import *  # noqa
 from .fit_model_for_peak_mass import *  # noqa
 from .petrel_age_predictor import *  # noqa
-from .plot_peak_mass_model import (
+from .plot_peak_mass_model import (  # noqa
     _plot_peak_mass_model_and_data,
     _plot_peak_mass_model_and_data_by_season,
     get_fitted_points,
-)  # noqa
+)
