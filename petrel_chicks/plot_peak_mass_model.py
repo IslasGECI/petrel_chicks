7df16c6b5037645fe336cce5a34249e0161dc489
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-11-11 19:49:18 +0000

    ♻️  Refactor

--- petrel_chicks/plot_peak_mass_model.py
+++ petrel_chicks/plot_peak_mass_model.py
@@ -14,14 +14,12 @@ def plot_all_peak_mass_models(df):
         filtered_data = df[df.Year == season]
         age, predicted_mass = get_fitted_points(filtered_data)
         plt.plot(age, predicted_mass)
-    setup_chicks_mass_vs_age_figure(fontsize)
-
-    legends = [f"Season {season}" for season in all_season]
-    plt.legend(legends)
+    _setup_chicks_mass_vs_age_figure(fontsize)
+    _write_season_legends(all_season)
     return ax
 
 
-def write_season_legends(all_season: list) -> None:
+def _write_season_legends(all_season: list) -> None:
     legends = [f"Season {season}" for season in all_season]
     plt.legend(legends)
 
@@ -44,12 +42,12 @@ def _plot_peak_mass_model_and_data(df):
     age, predicted_mass = get_fitted_points(df)
 
     plt.plot(age, predicted_mass, color="r")
-    setup_chicks_mass_vs_age_figure(fontsize)
+    _setup_chicks_mass_vs_age_figure(fontsize)
     plt.legend(["Measured bird mass", "Fitted model"])
     return ax
 
 
-def setup_chicks_mass_vs_age_figure(fontsize: int) -> None:
+def _setup_chicks_mass_vs_age_figure(fontsize: int) -> None:
     plt.ylabel("Mass $\\left( g \\right)$", fontsize=fontsize)
     plt.xlabel("Chick age $\\left( d \\right)$", fontsize=fontsize)
     plt.xticks(fontsize=fontsize)
