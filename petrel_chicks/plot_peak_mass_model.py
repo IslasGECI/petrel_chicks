6b10191f58f570d74edceeb41f9d93fb19518f9a
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-11-11 20:01:34 +0000

    ♻️  Refactor

--- petrel_chicks/plot_peak_mass_model.py
+++ petrel_chicks/plot_peak_mass_model.py
@@ -10,15 +10,11 @@ def plot_all_peak_mass_models(df):
     fontsize = 20
     _, ax = gp.geci_plot()
     all_season = _get_season_data(df)
-    _plot_model_for_all_seasons(df)
+    plotter = _Plotter_model_for_all_seasons(df)
+    plotter.plot_model_for_all_seasons()
     _setup_chicks_mass_vs_age_figure(fontsize)
     _write_season_legends(all_season)
     return ax
-
-
-def _plot_model_for_all_seasons(df):
-    plotter = _Plotter_model_for_all_seasons(df)
-    plotter.plot_model_for_all_seasons()
 
 
 class _Plotter_model_for_all_seasons:
