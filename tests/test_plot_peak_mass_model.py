c1d68bdcdc13c4a5ff5d3aef7fe3bdd033f3e97a
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-11-11 19:30:24 +0000

    ♻️  Refactor

--- tests/test_plot_peak_mass_model.py
+++ tests/test_plot_peak_mass_model.py
@@ -68,9 +68,8 @@ def test_plot_plot_all_peak_mass_models():
     obtained_ax = plot_all_peak_mass_models(df)
     plt.pyplot.savefig("test.png", transparent=True)
     first_children = 0
-    assert_children_is_line_2d(first_children, obtained_ax)
-    assert isinstance(obtained_ax.get_children()[1], plt.lines.Line2D)
-    assert isinstance(obtained_ax.get_children()[2], plt.lines.Line2D)
+    for nth_children in [0, 1, 2]:
+        assert_children_is_line_2d(nth_children, obtained_ax)
     assert obtained_ax.get_children()[0].get_data()[0][0] == 27
     assert obtained_ax.get_children()[1].get_data()[0][0] == 26
     assert obtained_ax.get_legend().get_texts()[0].get_text() == "Season 2013"
