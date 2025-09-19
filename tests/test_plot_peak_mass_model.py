e033efdc9a6e594949f10b86246d548aa757a346
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-09-19 16:46:11 +0000

    🛑🧪 Fail tests

--- tests/test_plot_peak_mass_model.py
+++ tests/test_plot_peak_mass_model.py
@@ -71,7 +71,7 @@ def test_plot_plot_all_peak_mass_models():
     for nth_children in children_index_for_each_season:
         child_nth = obtained_ax.get_children()[nth_children]
         assert_children_is_line_2d(child_nth)
-        assert_children_is_path_collection(child_nth + 1)
+        assert_children_is_path_collection(nth_children + 1)
     assert obtained_ax.get_children()[0].get_data()[0][0] == 27
     assert obtained_ax.get_children()[1].get_data()[0][0] == 26
     assert obtained_ax.get_legend().get_texts()[0].get_text() == "Season 2013"
